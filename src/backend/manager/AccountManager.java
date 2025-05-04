dcbf981bc73d475cd883806deb33bed1f8e80819
Karxariaz <enikologiannis@gmail.com>
2025-05-04 20:46:09 +0300

    peolara

--- src/backend/manager/AccountManager.java
+++ src/backend/manager/AccountManager.java
@@ -1,6 +1,16 @@
 package backend.manager;
 
 public class AccountManager {
+ 
+	private int peos;
 
+	public int getPeos() {
+		return peos;
+	}
+
+	public void setPeos(int peos) {
+		this.peos = peos;
+	}
+	
 }
 
