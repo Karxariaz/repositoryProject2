8abcce07da351e8e5e4f30d0ca065364812662b6
AsmoOriginal <nikolainathanail@gmail.com>
2025-05-04 20:39:26 +0300

    lol

--- src/backend/manager/AccountManager.java
+++ src/backend/manager/AccountManager.java
@@ -1,13 +1,6 @@
 package backend.manager;
 
 public class AccountManager {
-private int poutsa;
 
-public int getPoutsa() {
-	return poutsa;
 }
 
-public void setPoutsa(int poutsa) {
-	this.poutsa = poutsa;
-}
-}
