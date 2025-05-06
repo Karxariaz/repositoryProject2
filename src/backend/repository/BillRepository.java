8d1961ec188084fc91e002f8b65e1f7bd7f441bb
Karxariaz <enikologiannis@gmail.com>
2025-05-06 15:06:33 +0300

    jghmhg

--- src/backend/repository/BillRepository.java
+++ src/backend/repository/BillRepository.java
@@ -1,5 +1,16 @@
 package backend.repository;
 
 public class BillRepository {
+	
+	private int bill;
+
+	public int getBill() {
+		return bill;
+	}
+
+	public void setBill(int bill) {
+		this.bill = bill;
+	}
+	
 
 }
